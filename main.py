928cf7d1d958c704a06b54796c3c19663c13f889
Ling <sling102@amd.com>
2024-11-05 20:20:44 +0800

    Main

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.write("## POP ")
+st.write("Hello world")
+x=st.text_input("What's your name?")
+st.write(f"Your favourite movide is {x}")
+st.write("That is so funny! :joy:")
+is_clicked=st.button("Click me")
